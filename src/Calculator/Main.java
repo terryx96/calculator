7c97dd81dec95315316251a911860f5d36ba113a
Terry <Terry@LAPTOP-8FF5ALPJ.attlocal.net>
2018-05-28 15:04:59 -0500

    static variable body is now completely initialized within its declaration, buttons and text field to be added in their own method

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,11 +1,29 @@
 package Calculator;
 
+import java.awt.Component;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
 
 public class Main {
+	
 	public static void main(String...args) {
-		JFrame body = new JFrame("Calculator");
-		body.setSize(300, 485);
-		body.setVisible(true);
+		initializeCalculator();
+	}
+	
+	@SuppressWarnings("serial")
+	static JFrame body = new JFrame("Calculator") {{
+		setLayout(new FlowLayout());
+		setSize(300,485);
+		add(new JTextField(10));
+		setVisible(true);
+	}};
+	
+	private static void initializeCalculator() {
+		
 	}
 }
