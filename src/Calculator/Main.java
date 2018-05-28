286a1736c473bc66082d836344ba161cb182dd0b
Terry <Terry@LAPTOP-8FF5ALPJ.attlocal.net>
2018-05-28 14:39:09 -0500

    Initial Commit, added calculator body

---
+++ src/Calculator/Main.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String...args) {
+		JFrame body = new JFrame("Calculator");
+		body.setSize(300, 485);
+		body.setVisible(true);
+	}
+}
