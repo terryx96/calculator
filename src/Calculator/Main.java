40ff9bc836e8d428b995f1bb1873b4c44a4de187
Terry <Terry@LAPTOP-8FF5ALPJ.attlocal.net>
2018-05-29 02:19:36 -0500

    Added ButtonValue enum to play with

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -8,22 +8,28 @@ import javax.swing.JTextField;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class Main {
 	
 	public static void main(String...args) {
-		initializeCalculator();
+		initializeButtons();
 	}
 	
 	@SuppressWarnings("serial")
 	static JFrame body = new JFrame("Calculator") {{
 		setLayout(new FlowLayout());
 		setSize(300,485);
-		add(new JTextField(10));
+		add(new JTextField(20));
+		add(new JButton("hello"));
 		setVisible(true);
 	}};
 	
-	private static void initializeCalculator() {
-		
+	private static void initializeButtons() {
+		for(int i = 7; i > 0; i++) {
+			body.add(new JButton(new Integer(i).toString()));
+			if(i % 3 == 0) i -= 6;
+			
+		}
 	}
 }
